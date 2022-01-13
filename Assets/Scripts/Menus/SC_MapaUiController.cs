1ab9f7d117c31d6c4a2ad5817ed68d7660106900
Alexander Cirac Antio <95984829+AlexanderCirac@users.noreply.github.com>
2022-01-13 18:14:36 +0100

    Only refactoring 2.0.6
    Just refactor 2.0.6 and remove useless or redundant scripts

---
+++ Assets/Scripts/Menus/SC_MapaUiController.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using AL.UI.Settings;
+
+namespace AL.UI
+{
+    public class SC_MapaUiController : MonoBehaviour
+    {
+          #region Attributes
+          [Header("Button")]
+          [SerializeField] private Button _quitGame;
+          [System.Serializable] public class ButtonLevel {
+            public string _name;
+            public Button _buttonToLevel;
+            public int _intLevel;
+          }
+          [Header("Button Level")]
+          [SerializeField] private ButtonLevel[] _buttonLevel;
+          [System.Serializable] public class ShowButtons {
+            public string _name;
+            public Button _buttonToShow;
+          }
+          [Header("Show buttons")]
+          [SerializeField] private ShowButtons[] _showButtons;
+            private int _levelDataPlayer;
+          
+          //Main Tools
+          public SC_SettingUiController _settingsUIController;
+
+          #endregion
+
+          #region UnityCalls
+          void Start()
+          {
+              //Start game
+              Time.timeScale = 1;
+
+                //Applying button onClick 
+              for (int i = 1; i <= _buttonLevel.Length; i++)
+              {
+                  int _count = i;
+                  _buttonLevel[i-1]._buttonToLevel.onClick.AddListener(() => SceneManager.LoadScene(_buttonLevel[_count - 1]._intLevel));
+              }
+              _quitGame.onClick.AddListener(() => Application.Quit());
+
+              //Invoke 
+              Invoke(nameof(ShowButtonsDataPlayer),.2f);
+          }
+          #endregion
+
+          #region Methods
+          private void ShowButtonsDataPlayer()
+          {
+              //Activate Buttons Level
+              _levelDataPlayer = _settingsUIController._dataPlayer._levelCurrentSave;
+              for (int i = 1 ; i<= _levelDataPlayer; i++ )
+              {
+                  _showButtons[i-1]._buttonToShow.interactable = true;
+              }
+          }
+        #endregion
+    }
+}
