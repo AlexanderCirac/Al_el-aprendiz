dc36e7e1159607230b05fb0b02b7f8b25b6b42c1
Alexander Cirac Antio <95984829+AlexanderCirac@users.noreply.github.com>
2022-01-19 15:54:58 +0100

    Only refactoring 2.0.7
    Just refactor 2.0.7 and remove useless or redundant scripts

---
+++ Assets/Scripts/Menus/SC_NivelCinematicaUIController.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using AL.Data;
+
+namespace AL.UI
+{
+    public class SC_NivelCinematicaUIController : MonoBehaviour
+    {
+        #region Attributes
+        //[SerializeField] private Button _quitGame;
+        [System.Serializable]
+        public class ButtonLevel
+        {
+          public string _name;
+          public Button _buttonToLevel;
+          public int _intLevel;
+        }
+        [Header("Button")]
+        [SerializeField] private ButtonLevel[] _buttonLevel;
+        [Header("UI Settings")]
+        [SerializeField] private Image _brightness;
+        private AudioSource _music;
+        //Main Tools
+        [HideInInspector] private SC_SettingsDataPersisten _dataPlayer;
+        //bools
+        private bool _endCorrutine;
+        #endregion
+
+        #region UnityCalls
+        private void Awake()
+        {
+          _dataPlayer = SC_SettingsDataPersisten._instanceData;
+        }
+        private void Start()
+        {
+          _music = _dataPlayer.GetComponentInChildren<AudioSource>();
+
+          //Applying button onClick 
+          for (int i = 1; i <= _buttonLevel.Length; i++)
+          {
+            int _count = i;
+            _buttonLevel[i - 1]._buttonToLevel.onClick.AddListener(() => LoadingLevel(_count - 1));
+          }
+        
+          //corrutines
+          StartCoroutine(nameof(UpdateCorrutine));
+        }
+        private void OnDestroy()
+        {
+          _endCorrutine = true;
+        }
+
+        #endregion
+
+        #region Methods
+        IEnumerator UpdateCorrutine()
+        {
+          while (!_endCorrutine)
+          {
+            ApplicateUISettings();
+            yield return null;
+          }
+        }
+        private void ApplicateUISettings()
+        {
+          //brightness intensity controller
+          _brightness.color = new Color(_brightness.color.r, _brightness.color.g, _brightness.color.b, _dataPlayer._valueBrightness - 0.1f);
+          //Controll volum of music
+          _music.volume = _dataPlayer._valuenMusica;
+
+        }
+
+        private void LoadingLevel(int _int)
+        {
+          SceneManager.LoadScene(_buttonLevel[_int]._intLevel);
+        }
+        #endregion
+  
+    }
+}
