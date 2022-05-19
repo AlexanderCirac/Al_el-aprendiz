f938d208da15d75ede57bb614b7c1d31bdee407f
Alexander Cirac Antio <95984829+AlexanderCirac@users.noreply.github.com>
2022-05-19 11:20:44 +0200

    Only refactoring 2.0.7.6.2
    Just refactor 2.0.7.6.2 and remove useless or redundant scripts

--- Assets/Scripts/DataPlayer/SC_SettingsDataPersisten.cs
+++ Assets/Scripts/DataPlayer/SC_SettingsDataPersisten.cs
@@ -6,51 +6,45 @@ namespace AL.Data
     {
           #region Attributes
           //Data Settings
-          [HideInInspector] public int _levelCurrentSave;
-          [HideInInspector] public float _valueBrightness;
-          [HideInInspector] public float _valuenMusica;
-          [HideInInspector] public bool _handLeft;
-          [HideInInspector] public bool _ad; 
-          [HideInInspector] public bool _firstDLC;
-          [HideInInspector] public bool _isRetorno1;  
-          [HideInInspector] public bool _isRetorno2; 
-          [HideInInspector] public bool _isRetorno3;
+          internal int _levelCurrentSave;
+          internal float _valueBrightness;
+          internal float _valuenMusica;
+          internal bool _handLeft;
+          internal bool _ad; 
+          internal bool _firstDLC;
+          internal bool _isRetorno1;  
+          internal bool _isRetorno2; 
+          internal bool _isRetorno3;
           //main Tools
           [SerializeField ] private SC_SaveData _scriptable;
           #endregion
 
           #region Singleton
-          [HideInInspector]
-          public static SC_SettingsDataPersisten _instanceData;
+          internal static SC_SettingsDataPersisten _instanceData;
           #endregion
 
           #region UnityCalls
-          private void Awake()
-          {
-                if (_instanceData != null)
-                {
-                    Destroy(gameObject);
-                }
-                else 
-                { 
-                    _instanceData = this;
-                    DontDestroyOnLoad(gameObject);
-                }
-          }
-          void Start()
-          {
-              LoadSetting();
-          }
+          void Awake() => Init();
+          void Start() =>  LoadSetting();
           #endregion          
 
-          #region Methods
-          private void LoadSetting()
+          #region custom private methods
+          void LoadSetting()
           {
               _levelCurrentSave = _scriptable._savedLevel;
               _valueBrightness = _scriptable._saveValueBrighnesst;
               _valuenMusica = _scriptable._saveValueMusic;
               _handLeft = _scriptable._saveHandLeft;
-
+          }
+          void Init()
+          {
+                if (_instanceData != null)
+                    Destroy(gameObject);
+                else 
+                { 
+                    _instanceData = this;
+                    DontDestroyOnLoad(gameObject);
+                }
           }
           #endregion
     }
---
+++ Assets/Scripts/SC_GameGeneralMannager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace AL { 
+  public class SC_GameGeneralMannager : MyMonoBehaviour
+  {
+        #region Attributes
+        #endregion
+
+        #region UnityCalls
+        #endregion         
+    
+        #region custom private methods
+        #endregion 
+  }
+}
+
