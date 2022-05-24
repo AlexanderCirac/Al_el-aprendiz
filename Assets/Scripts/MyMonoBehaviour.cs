43b2b7629962613aaa2de42f564dee9c39608cdf
Alexander Cirac Antio <95984829+AlexanderCirac@users.noreply.github.com>
2022-05-24 13:59:07 +0200

    Only refactoring 2.0.7.6.3
    Just refactor 2.0.7.6.3 and remove useless or redundant scripts

--- Assets/Scripts/MyMonoBehaviour.cs
+++ Assets/Scripts/MyMonoBehaviour.cs
@@ -1,13 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-namespace AL
-{
-    using AL.Data;
-    public class MyMonoBehaviour : MonoBehaviour
+namespace AL {
+  using AL.Data;
+  public class MyMonoBehaviour : MonoBehaviour
     {
-       protected SC_GameGeneralMannager _gameManager => SC_GameGeneralMannager._instance;
-       protected SC_SettingsDataPersisten _settingData => _gameManager._settingsDataPersisten;
-       protected SC_SaveAndLoadPlayerData _playerData => _gameManager._saveAndLoadPlayerData;
 
-    } 
+    //protected SC_GameGeneralMannager _gameManager => SC_GameGeneralMannager._instance;
+    //protected SC_SettingsDataPersisten _settingData => _gameManager._settingsDataPersisten;
+    //protected SC_SaveAndLoadPlayerData _playerData => _gameManager._saveAndLoadPlayerData;
+  } 
 }
+
