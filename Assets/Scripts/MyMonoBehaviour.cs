fec93cb5d3ee0f300e4c1615f3c86c2efd8c8bec
Alexander Cirac Antio <95984829+AlexanderCirac@users.noreply.github.com>
2022-05-26 19:32:07 +0200

    Only refactoring 2.0.7.6.4
    Just refactor 2.0.7.6.4 and remove useless or redundant scripts

--- Assets/Scripts/MyMonoBehaviour.cs
+++ Assets/Scripts/MyMonoBehaviour.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace AL {
-  using AL.Data;
+ using AL.Data;
   public class MyMonoBehaviour : MonoBehaviour
     {
 
-    //protected SC_GameGeneralMannager _gameManager => SC_GameGeneralMannager._instance;
+    protected SC_GameGeneralMannager _gameManager => SC_GameGeneralMannager._instance;
     //protected SC_SettingsDataPersisten _settingData => _gameManager._settingsDataPersisten;
     //protected SC_SaveAndLoadPlayerData _playerData => _gameManager._saveAndLoadPlayerData;
   } 
--- Assets/Scripts/SC_GameGeneralMannager.cs
+++ Assets/Scripts/SC_GameGeneralMannager.cs
@@ -1,28 +1,28 @@
-namespace AL 
+namespace AL
 {
-    using AL.Data;
-    public class SC_GameGeneralMannager : MyMonoBehaviour
+  using AL.Data;
+  public class SC_GameGeneralMannager : MyMonoBehaviour
+  {
+    #region Attributes
+    //internal SC_SettingsDataPersisten _settingsDataPersisten;
+    //internal SC_SaveAndLoadPlayerData _saveAndLoadPlayerData;
+    internal int i;
+    internal static SC_GameGeneralMannager _instance;
+
+    #endregion
+
+    #region UnityCalls
+    void Awake() => Init();
+    #endregion
+
+    #region custom private methods
+    void Init()
     {
-          #region Attributes
-          internal SC_SettingsDataPersisten _settingsDataPersisten;
-          internal SC_SaveAndLoadPlayerData _saveAndLoadPlayerData;
-
-          public static SC_GameGeneralMannager _instance;
-
-          #endregion
-
-          #region UnityCalls
-          void Awake() => Init();
-          #endregion
-
-          #region custom private methods
-          void Init()
-          {
-              _instance = this;
-              _settingsDataPersisten = FindObjectOfType<SC_SettingsDataPersisten>();
-              _saveAndLoadPlayerData = FindObjectOfType<SC_SaveAndLoadPlayerData>();
-          }
-          #endregion
+      _gameManager.i = 0;
+      _instance = this;
+      //_settingsDataPersisten = FindObjectOfType<SC_SettingsDataPersisten>();
+      //_saveAndLoadPlayerData = FindObjectOfType<SC_SaveAndLoadPlayerData>();
     }
+    #endregion
+  }
 }
-
