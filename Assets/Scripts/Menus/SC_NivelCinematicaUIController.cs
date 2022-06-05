da33ab6b17b5adb7123223b75afaa6776ce8d511
Alexander Cirac Antio <95984829+AlexanderCirac@users.noreply.github.com>
2022-06-05 13:35:36 +0200

    Only refactoring 2.0.7.6.11
    Just refactor 2.0.7.6.11 and remove useless or redundant scripts

--- Assets/Scripts/Menus/SC_NivelCinematicaUIController.cs
+++ Assets/Scripts/Menus/SC_NivelCinematicaUIController.cs
@@ -6,74 +6,69 @@ using AL.Data;
 
 namespace AL.UI
 {
-    public class SC_NivelCinematicaUIController : MyMonoBehaviour
+  public class SC_NivelCinematicaUIController : MyMonoBehaviour
+  {
+    #region Attributes
+    //[SerializeField] private Button _quitGame;
+    [System.Serializable]
+    public class ButtonLevel
     {
-        #region Attributes
-        //[SerializeField] private Button _quitGame;
-        [System.Serializable]
-        public class ButtonLevel
-        {
-          [SerializeField] private string _name;
-          public Button _button;
-          public int _iDLevel;
-        }
-        [Header("Button")]
-        [SerializeField] private ButtonLevel[] _arrayLevelButton;        
-        [SerializeField] private Button _creditButton;        
-        [Header("UI Settings")]
-        [SerializeField] private Image _brightnessPanel;
-        private AudioSource _music;
-        [Header("Panel")]
-        [SerializeField] private GameObject _cinematicaPanel;
-        //Main Tools
-        [HideInInspector] private SC_SettingsDataPersisten _dataPlayer;
-
-        #endregion
-
-        #region UnityCalls
-        private void Awake()
-        {
-          //_dataPlayer = SC_SettingsDataPersisten._instanceData;
-        }
-        private void Start()
-        {
-          _music = _dataPlayer.GetComponentInChildren<AudioSource>();
-
-          //Applying button onClick 
-          for (int i = 1; i <= _arrayLevelButton.Length; i++)
-          {
-            int _count = i;
-            _arrayLevelButton[i - 1]._button.onClick.AddListener(() => ToLoadLevel(_count - 1));
-          }
-          _creditButton.onClick.AddListener(() => SceneManager.LoadScene(1));
-          //Invocke
-          Invoke(nameof(ToShowCinematic),.4f);
-        }
-        private void Update()
-        {
-            ToUISettings();
-        }
-        #endregion
-
-        #region Methods
-        private void ToUISettings()
-        {
-          //brightness intensity controller
-          _brightnessPanel.color = new Color(_brightnessPanel.color.r, _brightnessPanel.color.g, _brightnessPanel.color.b, _dataPlayer._valueBrightness - 0.1f);
-          //Controll volum of music
-          _music.volume = _dataPlayer._valuenMusica;
-
-        }
-
-        private void ToLoadLevel(int _iDArray)
-        {
-          SceneManager.LoadScene(_arrayLevelButton[_iDArray]._iDLevel);
-        }        
-        private void ToShowCinematic()
-        {
-          _cinematicaPanel.SetActive(true);
-        }
-        #endregion
-  
+      [SerializeField] private string _name;
+      public Button _button;
+      public int _iDLevel;
     }
+    [Header("Button")]
+    [SerializeField] private ButtonLevel[] _arrayLevelButton;
+    [SerializeField] private Button _creditButton;
+    [Header("UI Settings")]
+    [SerializeField] private Image _brightnessPanel;
+    private AudioSource _music;
+    [Header("Panel")]
+    [SerializeField] private GameObject _cinematicaPanel;
+
+
+    #endregion
+
+    #region UnityCalls
+    private void Start()
+    {
+      _music = _settingData.GetComponentInChildren<AudioSource>();
+
+      //Applying button onClick 
+      for (int i = 1; i <= _arrayLevelButton.Length; i++)
+      {
+        int _count = i;
+        _arrayLevelButton[i - 1]._button.onClick.AddListener(() => ToLoadLevel(_count - 1));
+      }
+      _creditButton.onClick.AddListener(() => SceneManager.LoadScene(1));
+      //Invocke
+      Invoke(nameof(ToShowCinematic), .4f);
+    }
+    private void Update()
+    {
+      ToUISettings();
+    }
+    #endregion
+
+    #region Methods
+    private void ToUISettings()
+    {
+      //brightness intensity controller
+      _brightnessPanel.color = new Color(_brightnessPanel.color.r, _brightnessPanel.color.g, _brightnessPanel.color.b, _settingData._valueBrightness - 0.1f);
+      //Controll volum of music
+      _music.volume = _settingData._valuenMusica;
+
+    }
+
+    private void ToLoadLevel(int _iDArray)
+    {
+      SceneManager.LoadScene(_arrayLevelButton[_iDArray]._iDLevel);
+    }
+    private void ToShowCinematic()
+    {
+      _cinematicaPanel.SetActive(true);
+    }
+    #endregion
+
+  }
 }
