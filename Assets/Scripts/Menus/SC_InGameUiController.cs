3cbb8487d53f7cbf0d51d34c600db01ae36b5482
Alexander Cirac Antio <95984829+AlexanderCirac@users.noreply.github.com>
2022-06-08 18:31:33 +0200

    Only refactoring 2.0.7.6.14
    Just refactor 2.0.7.6.14 and remove useless or redundant scripts

--- Assets/Scripts/Menus/SC_InGameUiController.cs
+++ Assets/Scripts/Menus/SC_InGameUiController.cs
@@ -8,60 +8,57 @@ using AL.Data;
 
 namespace AL.UI
 {
-  public class SC_InGameUiController : MyMonoBehaviour
-  {
-    #region Attributes
-    [Header("int")]
-    [SerializeField] private int _currentLevel;
-    [Header("Buttons")]
-    [SerializeField] private Button _winButton;
-    [System.Serializable]
-    public class ButtonsToCancelMenu
+    public class SC_InGameUiController : MyMonoBehaviour
     {
-      public Button[] _button;
-    }
-    [SerializeField] private ButtonsToCancelMenu _buttonsToCancelMenu;
-    [Header("Panels")]
-    [SerializeField] private GameObject _loadingPanel;
-    [SerializeField] private GameObject _menuButtonPanel;
-    // private bool _loadingPanel;
+        #region Attributes
+        [Header("int")]
+        [SerializeField] private int _currentLevel;
+        [Header("Buttons")]
+        [SerializeField] private Button _winButton;
+        [System.Serializable]
+        public class ButtonsToCancelMenu
+        {
+          public Button[] _button;
+        }
+        [SerializeField] private ButtonsToCancelMenu _buttonsToCancelMenu;
+        [Header("Panels")]
+        [SerializeField] private GameObject _loadingPanel;
+        [SerializeField] private GameObject _menuButtonPanel;
+        #endregion
 
-    #endregion
+        #region UnityCalls
+        void Awake() => Init();
+        #endregion
 
-    #region UnityCalls
-    void Start()
-    {
-      //Buttons
-      _winButton.onClick.AddListener(ToWin);
-      for (int i = 1; i <= _buttonsToCancelMenu._button.Length; i++)
-      {
-        _buttonsToCancelMenu._button[i - 1].GetComponent<SC_Touch>().OnHold.AddListener(ToShowMenu);
-        _buttonsToCancelMenu._button[i - 1].GetComponent<SC_Touch>().OnRealise.AddListener(ToHiddenMenu);
-      }
-
+        #region custom private methods
+        void Init()
+        {
+            //Buttons
+            _winButton.onClick.AddListener(ToWin);
+            for (int i = 1; i <= _buttonsToCancelMenu._button.Length; i++)
+            {
+              _buttonsToCancelMenu._button[i - 1].GetComponent<SC_Touch>().OnHold.AddListener(ToShowMenu);
+              _buttonsToCancelMenu._button[i - 1].GetComponent<SC_Touch>().OnRealise.AddListener(ToHiddenMenu);
+            }
+        }
+        void ToWin()
+        {
+            if (_currentLevel > _settingData._levelCurrentSave)
+            {
+              _settingData._levelCurrentSave++;
+              _playerData.Save();
+            }
+            _loadingPanel.SetActive(true);
+            SceneManager.LoadScene(2);
+        }
+        void ToShowMenu()
+        {
+            _menuButtonPanel.SetActive(true);
+        }
+        void ToHiddenMenu()
+        {
+            _menuButtonPanel.SetActive(false);
+        }
+        #endregion
     }
-    #endregion
-
-    #region Methods
-    private void ToWin()
-    {
-      if (_currentLevel > _settingData._levelCurrentSave)
-      {
-        _settingData._levelCurrentSave++;
-        _playerData.Save();
-      }
-      _loadingPanel.SetActive(true);
-      SceneManager.LoadScene(2);
-    }
-    private void ToShowMenu()
-    {
-      _menuButtonPanel.SetActive(true);
-    }
-    private void ToHiddenMenu()
-    {
-      _menuButtonPanel.SetActive(false);
-    }
-    #endregion
-  }
-
 }
