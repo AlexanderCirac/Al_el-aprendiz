6a20291778b8d6af8c1dfa19926c92862ec0234b
Alexander Cirac Antio <95984829+AlexanderCirac@users.noreply.github.com>
2022-06-15 11:24:36 +0200

    Only refactoring 2.0.7.7.2
    Just refactor 2.0.7.7.2

--- Assets/Scripts/InGame/Player/SC_PlayerController.cs
+++ Assets/Scripts/InGame/Player/SC_PlayerController.cs
@@ -7,52 +7,29 @@ namespace AL.Player
   public class SC_PlayerController : SC_PlayerBehaviour
   {
       #region
-      public Rigidbody m_rg;
-      public GameObject jugador;
+      [SerializeField] private Rigidbody m_rg;
+      [SerializeField] private GameObject jugador;
       public float m_velocidadMovimiento;
       public float m_velocidadSaltar;
-      public bool m_agachar;
-      private bool m_liana = false;
-
-      [HideInInspector]
-      public float m_normalY;
-      private float m_normalZ;
-      
-      [HideInInspector]
-      public bool m_m1;
-
-      public float m_tiempo;
-      private bool m_tiempoControl;
+     
+      float m_tiempo;
 
       //estas funciones son para activare el movimiento durante el ascensor
-      public bool m_ascensor;
+      internal bool m_ascensor;
       #endregion
 
       #region Unity Calls
-      private void Start()
-      {
-    
-        m_normalY = jugador.transform.localScale.y;
-        m_normalZ = jugador.transform.localScale.z;
- 
-      }
-      private void Update()
-      {
-   
-    
-        Saltarboton();
-      }
+      void Update() => Saltarboton();
       #endregion
       
       #region custom public methods
       public void MoverDercha()
       {
 
-          // m_rg.MovePosition(new Vector3(jugador.transform.position.x + m_velocidadMovimiento, jugador.transform.position.y, jugador.transform.position.z));
+       
           if (!m_ascensor)
           {
             m_rg.MovePosition(new Vector3(jugador.transform.position.x + m_velocidadMovimiento * Time.deltaTime, jugador.transform.position.y, jugador.transform.position.z));
-            //jugador.transform.position = new Vector3(jugador.transform.position.x + (m_velocidadMovimiento/2), jugador.transform.position.y, jugador.transform.position.z);
           }
           else
           {
@@ -65,43 +42,33 @@ namespace AL.Player
       public void MoverIzquierda()
       {
 
-          //m_rg.MovePosition (new Vector3(jugador.transform.position.x - m_velocidadMovimiento, jugador.transform.position.y, jugador.transform.position.z));// forma 1 ( movimiento con fisica pero sin deslizamiento)
+
           if (!m_ascensor)
           {
-            m_rg.MovePosition(new Vector3(jugador.transform.position.x - m_velocidadMovimiento * Time.deltaTime, jugador.transform.position.y, jugador.transform.position.z));// forma 1 ( movimiento con fisica pero sin deslizamiento)
+            m_rg.MovePosition(new Vector3(jugador.transform.position.x - m_velocidadMovimiento * Time.deltaTime, jugador.transform.position.y, jugador.transform.position.z));// form
           }
           else
           {
-            jugador.transform.position =  new Vector3(jugador.transform.position.x -( (m_velocidadMovimiento /2) * Time.deltaTime) , jugador.transform.position.y, jugador.transform.position.z);// forma 1 ( movimiento con fisica pero sin deslizamiento)
+            jugador.transform.position =  new Vector3(jugador.transform.position.x -( (m_velocidadMovimiento /2) * Time.deltaTime) , jugador.transform.position.y, jugador.transform.position.z);
           }
-          //m_rg.velocity = Vector3.left * m_velocidadMovimiento; // forma 2 ( movimiento con fisica pero tiene un deslizamiento)
-          //jugador.transform.position = new Vector3(jugador.transform.position.x - m_velocidadMovimiento * Time.deltaTime, jugador.transform.position.y, jugador.transform.position.z);// forma 3 ( movimiento sin fisica)
-    
       }
 
       public void Saltar()
       {
-        //m_rg.AddForce ( Vector3.up * m_velocidadSaltar);
         if (_jumpController.m_boolSalto == true)
         {
-
           m_rg.velocity = Vector3.up * m_velocidadSaltar  ;
-          //m_rg.velocity = Vector3.up * m_velocidadSaltar  *Time.deltaTime;
           m_tiempo = 0;
         }
       }
       #endregion
 
       #region custom private methods
-      
       void Saltarboton()
       {
-        //m_rg.AddForce ( Vector3.up * m_velocidadSaltar);
         if (_jumpController.m_boolSalto == true && Input.GetKeyDown(KeyCode.Space))
         {
-
           m_rg.velocity = Vector3.up * m_velocidadSaltar ;
-          //m_rg.velocity = Vector3.up * m_velocidadSaltar *Time.deltaTime;
           m_tiempo = 0;
         }
       }
@@ -111,13 +78,11 @@ namespace AL.Player
         if(_jumpController.m_boolSalto == false)
         {
           m_tiempo = m_tiempo + 2 * Time.deltaTime;
-          // m_tiempo ++;
             if (m_tiempo > 2)
             {
               m_rg.velocity = Vector3.down * (m_velocidadSaltar/1.1f);
             }
         }
-
       }
       #endregion
   }
