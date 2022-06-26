173b3fa8ecfef273d7f82016ab685928f243a742
Alexander Cirac Antio <95984829+AlexanderCirac@users.noreply.github.com>
2022-06-26 09:15:36 +0200

    Only refactoring 2.0.7.18
    Just refactor 2.0.7.18

---
+++ Assets/Scripts/InGame/Controls/SC_FlipController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SC_FlipController : MonoBehaviour
+{
+
+  #region Attribute
+  [SerializeField] SpriteRenderer _flipPlayer;
+  [SerializeField] Button _leftButton;
+  [SerializeField] Button _rightButton;
+  #endregion
+
+  #region UnityCalls
+  void Awake() => Init();
+  #endregion
+
+  #region custom private method
+  void Init()
+  {
+    _leftButton.onClick.AddListener(() => _flipPlayer.flipX = false);
+    _rightButton.onClick.AddListener(() => _flipPlayer.flipX = true);
+  }
+  #endregion
+}
