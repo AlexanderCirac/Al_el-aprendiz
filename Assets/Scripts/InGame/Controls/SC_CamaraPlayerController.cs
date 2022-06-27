d3abe531db24913141ca9d2f5be6659202bd87a0
Alexander Cirac Antio <95984829+AlexanderCirac@users.noreply.github.com>
2022-06-27 13:29:44 +0200

    Only refactoring 2.0.7.19
    Just refactor 2.0.7.19

---
+++ Assets/Scripts/InGame/Controls/SC_CamaraPlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+
+namespace AL.Camara
+{
+
+    public class SC_CamaraPlayerController : MonoBehaviour
+    {
+
+        #region Attributes
+        public GameObject _camera;
+        public float _velocity;
+        #endregion
+
+        #region UnityCall
+        private void OnTriggerEnter(Collider coll)
+        {
+          if (coll.CompareTag("Suelo2"))
+            ZoomOut();
+
+          if (coll.CompareTag("Suelo1"))
+            ZoomIn();
+        }
+        #endregion
+
+        #region custom private methods
+        void ZoomIn()
+        {
+          while( _camera.GetComponent<Camera>().fieldOfView >= 35)
+            _camera.GetComponent<Camera>().fieldOfView -= _velocity * Time.deltaTime;
+        }
+        void ZoomOut()
+        {
+            while(_camera.GetComponent<Camera>().fieldOfView <= 50)
+                _camera.GetComponent<Camera>().fieldOfView += _velocity * Time.deltaTime;
+        }
+        #endregion
+
+
+
+    }
+}
+
