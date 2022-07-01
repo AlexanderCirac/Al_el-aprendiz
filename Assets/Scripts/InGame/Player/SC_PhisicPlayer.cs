28fbef6cb48f9e6723640ccf8ee6284e49d3fc70
Alexander Cirac Antio <95984829+AlexanderCirac@users.noreply.github.com>
2022-07-01 12:59:11 +0200

    Only refactoring 2.0.7.23
    Just refactor 2.0.7.23

---
+++ Assets/Scripts/InGame/Player/SC_PhisicPlayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+
+namespace AL.Player
+{
+
+  public class SC_PhisicPlayer : MonoBehaviour
+  {
+
+      #region Attribute
+      public SC_JumpController SC_CA;
+      public PhysicMaterial m_PM;    
+      #endregion
+
+      #region UnityCall
+      void Update() => controlfisicas();
+      #endregion
+
+      #region custom private methods
+      void controlfisicas()
+      {
+        if (SC_CA._isJumping == false)
+        {
+          m_PM.dynamicFriction = 0.1f;
+        }
+        else
+        {
+          m_PM.dynamicFriction = 1;
+        }
+      }
+      #endregion
+
+  }
+}
