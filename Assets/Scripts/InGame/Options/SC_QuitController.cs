7ea0b4bd05f04fe3d884b65d2f7b2cd2d77fb8fb
Alexander Cirac Antio <95984829+AlexanderCirac@users.noreply.github.com>
2022-07-12 10:41:03 +0200

    Only refactoring 2.0.7.30
    Only refactoring 2.0.7.30

---
+++ Assets/Scripts/InGame/Options/SC_QuitController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace AL.InGame.UI
+{
+  public class SC_QuitController : MonoBehaviour
+  {
+    #region Attributes
+    public GameObject m_mesanje;
+    #endregion
+
+    #region UnityCalls
+    #endregion
+
+    #region custom private methods
+    public void Salir()
+    {
+      Application.Quit();
+    }
+
+    public void ActivarSalir()
+    {
+      m_mesanje.SetActive(true);
+    }
+    public void DesactivarSalir()
+    {
+      m_mesanje.SetActive(false);
+    }
+
+    public void NivelEleccion(int m_nivel)
+    {
+      Application.LoadLevel(m_nivel);
+    }
+    #endregion
+
+    
+
+  }
+}
