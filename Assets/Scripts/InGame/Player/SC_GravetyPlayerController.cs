bb0d03ebc5e05e7b7f4f7a0bd7c090bd5e263caf
Alexander Cirac Antio <95984829+AlexanderCirac@users.noreply.github.com>
2022-07-28 14:12:42 +0200

    Only refactoring 2.0.9.5
    Only refactoring 2.0.9.5

--- Assets/Scripts/InGame/Player/SC_GravetyPlayerController.cs
+++ Assets/Scripts/InGame/Player/SC_GravetyPlayerController.cs
@@ -7,65 +7,41 @@ namespace AL.Player
     public class SC_GravetyPlayerController : MonoBehaviour
     {
         #region Attributes
-        //Update is called once per frame
-        //public float m_gravedad;
-        //public bool m_gravedad1;
-        //public SC_JugadorMovimiento SC_J;
-        //public float m_velocidadMovimiento;
-
-        //  public GameObject m_jugador;
-
-        private float m_velocidadestandar;
-        private bool m_gravedad;
-        //private bool m_desgravedad;
-        public SC_PlayerController SC_J;
-        //public SC_jugadorEfectoDaño SC_JED;
-        private float m_velocidadNormal;
-        public float m_massa;
-        public float m_fuerza;
-        private float m_fuerzanormal;
+        bool m_gravedad;
+        [SerializeField] SC_PlayerController SC_J;
+        [SerializeField] float m_massa;
+        [SerializeField] float m_fuerza;
+        float m_fuerzanormal;
         #endregion   
     
         #region UnityCalls
         // Start is called before the first frame update
-        void Start()
-        {
-        m_fuerzanormal = this.GetComponent<ConstantForce>().force.y;
-        if (SC_J != null)
-        {
-
-        m_velocidadestandar = SC_J.m_velocidadMovimiento;
-        }
-        }
-
-        private void Update()
-        {
-          gravedad();
-        }
+        void Start()=> Init();
         private void OnTriggerEnter(Collider coll)
         {
 
           if (coll.CompareTag("Caida"))
-          {
-            m_gravedad = true;
-          }
+            gravedad(true);
           if (coll.CompareTag("caida1"))
-          {
-
-            m_gravedad = false;
-          }
+            gravedad(false);
         }
     #endregion
 
     #region custom private methods
-    void gravedad()
+    void Init()
     {
+      m_fuerzanormal = this.GetComponent<ConstantForce>().force.y;
+    }
+    void gravedad(bool _state)
+    {
+
       if (m_gravedad)
       {
-
-        this.GetComponent<Rigidbody>().mass = m_massa;
-        this.GetComponent<ConstantForce>().force = new Vector3(0, m_fuerza, 0);
-
+        while (m_gravedad)
+        {
+            this.GetComponent<Rigidbody>().mass = m_massa;
+            this.GetComponent<ConstantForce>().force = new Vector3(0, m_fuerza, 0);
+        }
       }
       else
       {
